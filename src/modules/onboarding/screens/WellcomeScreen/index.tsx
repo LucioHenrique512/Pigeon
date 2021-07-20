362fd4928f654dc4583e21d144d8d6415dad3e5e
Lúcio Henrique <lucions96@gmail.com>
2021-07-19 21:53:25 -0300

    iniciando modulo de onboarding

---
+++ src/modules/onboarding/screens/WellcomeScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from './styles';
+import {Text} from 'react-native';
+
+export const WellcomeScreen = () => {
+  return (
+    <Container>
+      <Text>Ola de wellcome</Text>
+    </Container>
+  );
+};
